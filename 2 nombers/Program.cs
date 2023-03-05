c5731513464ca30bca68dfbb78735c06f2efea5a
Maxim <pmv@bk.ru>
2023-03-05 22:57:08 +0300

    добавили в сумму 2-х чисел рандомный ввыбор A и B

--- 2 nombers/Program.cs
+++ 2 nombers/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA = 10;
-int numberM = 13;
-int result = (numberA + numberM);
+﻿int numberA = new Random(). Next(1, 10); // число случайное 1, 2, 3, 4, ...., 9.
+Console.WriteLine(numberA);
+int numberB = new Random(). Next(1, 10);
+Console.WriteLine(numberB);
+int result = (numberA + numberB);
 Console.WriteLine (result);
