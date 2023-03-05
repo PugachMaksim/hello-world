422633f7749fb5cffef18dcef9245c15426c07ad
Maxim <pmv@bk.ru>
2023-03-05 23:47:28 +0300

    добавили задачу по поиску максимального числа

---
+++ 6 example/Program.cs
@@ -0,0 +1,16 @@
+﻿int a = 3;
+int b = 6;
+int c = 1;
+int d = 8;
+int f = 2;
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (f > max) max = f;
+
+Console.Write ("max = ");
+Console.WriteLine(max);
