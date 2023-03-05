e792a5788b852fc5951f7bf65f43064d06aef4c6
Maxim <pmv@bk.ru>
2023-03-05 23:33:18 +0300

    Добавили индивидуальное приветствие по имени

---
+++ 5 example/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "Maxim")
+{
+    Console.WriteLine("Ура, это же Maxim");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
